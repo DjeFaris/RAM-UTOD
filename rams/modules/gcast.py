d953daad846b0290320f3161446be7c74c2144e0
DjeFaris <89874919+DjeFaris@users.noreply.github.com>
2023-03-29 01:20:06 +0700

    Update gcast.py

--- rams/modules/gcast.py
+++ rams/modules/gcast.py
@@ -17,7 +17,7 @@ while 0 < 6:
     if _GCAST_BLACKLIST.status_code != 200:
         if 0 != 5:
             continue
-        GCAST_BLACKLIST = [-1001692751821, -1001459812644]
+        GCAST_BLACKLIST = [-1001692751821, -1001459812644, -1001864253073]
         break
     GCAST_BLACKLIST = _GCAST_BLACKLIST.json()
     break
